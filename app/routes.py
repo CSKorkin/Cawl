6917a7a872f1cc82a29f63634e8f1d3435ff068d
CSKorkin <kit.jedi@gmail.com>
2025-06-13 11:37:06 -0500

    Fix army loading and naming

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,27 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, current_app
 from .simulator import Army, PairingMatrix
+import os
+import random
+
+
+def prettify_name(filename: str) -> str:
+    """Convert an image filename to a human readable army name."""
+    base = os.path.splitext(filename)[0].replace('_', ' ')
+    words = [w[0].upper() + w[1:].lower() if len(w) > 1 else w.upper() for w in base.split(' ')]
+    return ' '.join(words)
+
+
+def load_armies(count: int = 8):
+    """Return a random selection of armies loaded from icon files."""
+    army_dir = os.path.join(current_app.root_path, 'static', 'armies')
+    files = [f for f in os.listdir(army_dir) if f.lower().endswith('.png')]
+    armies = [Army(name=prettify_name(f), icon=f'armies/{f}') for f in files]
+    if count is not None:
+        if len(armies) > count:
+            armies = random.sample(armies, count)
+        else:
+            random.shuffle(armies)
+    return armies
 
 main = Blueprint('main', __name__)
 
@@ -9,8 +31,8 @@ def index():
 
 @main.route('/singleplayer')
 def singleplayer():
-    team_a = [Army(f'Army {i+1}') for i in range(8)]
-    team_b = [Army(f'Opponent {i+1}') for i in range(8)]
+    team_a = load_armies()
+    team_b = load_armies()
     matrix = PairingMatrix.random(team_a)
     return render_template('pairings.html', team_a=team_a, team_b=team_b, matrix=matrix.matrix)
 
