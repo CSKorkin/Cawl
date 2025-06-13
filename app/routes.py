bf11d41ea7604eae41e1f50a50d14da9a14506df
spyder-kit <kit.smith.hanna@spyderbat.com>
2025-06-13 10:01:05 -0500

    added initial UI

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,14 @@
 from flask import Blueprint, render_template
+from .simulator import Army, PairingMatrix
 
 main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
     return render_template('index.html')
+
+@main.route('/singleplayer')
+def singleplayer():
+    armies = [Army(f'Army {i+1}') for i in range(8)]
+    matrix = PairingMatrix.random(armies)
+    return render_template('singleplayer.html', matrix=matrix.matrix)
